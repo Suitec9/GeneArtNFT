f5a7cd1f0130bee817b0a161fbfe3e9c3e67e2b0
SSuitec99Double <104209435+Suitec9@users.noreply.github.com>
2023-08-08 02:20:11 +0200

    #initial coms

---
+++ geneartapp/app/page.js
@@ -0,0 +1,8 @@
+import Sketch1 from '../sketchs/sketch1';
+
+const Homepage = () => {
+
+  return (
+    <div><Sketch1/></div>
+  )
+}
---
+++ geneartapp/sketches/p5instance.js
@@ -0,0 +1,12 @@
+const sketch = p => {
+    
+    p.setup = () => {
+        p.createCanvas(500, 500);
+    }
+
+    p.draw = () => {
+        // generative algorithms
+    }
+}
+
+export default sketch;
